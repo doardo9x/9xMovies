0217ce404adc2948278f5df9980a79de4163bc2b
doardo9x <eduardo010304@gmail.com>
2024-11-04 11:39:13 -0300

    04/11

--- Gmovies/navigation/appNavigation.js
+++ Gmovies/navigation/appNavigation.js
@@ -4,12 +4,11 @@ import HomeScreen from '../screens/HomeScreen';
 
 const Stack = createNativeStackNavigator();
 
-
 export default function AppNavigation() {
   return (
     <NavigationContainer>
         <Stack.Navigator>
-            <Stack.Screen name='Home' component={HomeScreen} options={{headerShown: false}}/>
+            <Stack.Screen name='Home' component={HomeScreen} options={{headerShown: false}} />
         </Stack.Navigator>
     </NavigationContainer>
   )
